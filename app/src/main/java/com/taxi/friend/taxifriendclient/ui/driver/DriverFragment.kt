e477bfaf2ca7754982b20f72f2688853f0c85f47
Alex Ventura <alex.rv11@gmail.com>
2020-01-28 16:18:01 -0300

    implement gallery

--- app/src/main/java/com/taxi/friend/taxifriendclient/ui/driver/DriverFragment.kt
+++ app/src/main/java/com/taxi/friend/taxifriendclient/ui/driver/DriverFragment.kt
@@ -1,10 +1,9 @@
 package com.taxi.friend.taxifriendclient.ui.driver
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.util.Log
+
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,17 +11,9 @@ import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.SnapHelper
-
 import com.taxi.friend.taxifriendclient.R
-import com.taxi.friend.taxifriendclient.models.Driver
 import com.taxi.friend.taxifriendclient.models.DriverData
-import com.taxi.friend.taxifriendclient.models.ResponseWrapper
-import com.taxi.friend.taxifriendclient.services.DriverService
 import kotlinx.android.synthetic.main.driver_fragment.view.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.lang.Exception
 
 class DriverFragment : Fragment() {
     val driverId: String = "070f886c-ae24-4a2d-a252-9f9c3df7c399"
